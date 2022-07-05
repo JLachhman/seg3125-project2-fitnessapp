5ce1a600eec0f8065017a856858b4017879d2f35
JLachhman <jlach107@uottawa.ca>
2022-07-05 15:04:02 -0400

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+export const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button 
+          color={showAdd ? 'grey' : 'black'} 
+          text={showAdd ? 'Close' : 'Add'} 
+          onClick={onAdd}/>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Fitness App'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
